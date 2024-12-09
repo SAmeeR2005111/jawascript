d73f160ada4153b3418c325c0b15ce871ef140e5
SAmeeR2005111 <sameerydv1920@gmail.com>
2024-12-09 15:41:11 +0530

    Printed even no. from 0-100.

--- first.js
+++ first.js
@@ -1,8 +1,13 @@
-console.log("fullName");
-let Identity = prompt;
-console.log(prompt("Enter your name"),Identity);
+console.log("Welcome");
 
 // print all the no. from 0-100.
 for(i=0;i<=100;i++){
     console.log(i);
+}
+
+//print all the even no. from 0-100.
+for( num = 0 ; num <= 100 ; num++ ) {
+    if(num%2===0) {
+      console.log("Even no. =",num);
+    }
 }
