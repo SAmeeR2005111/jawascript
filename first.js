73f25e2f10bb37b2e35218f922355bf56e1185b4
SAmeeR2005111 <sameerydv1920@gmail.com>
2024-12-09 20:28:10 +0530

    generated a username using prompt which start with '@' ,followed by the entered username and end with the username length.

--- first.js
+++ first.js
@@ -13,10 +13,19 @@ console.log("Welcome");
 //}
 
 /*creating a game where you start with any random game number. 
-Ask the user to keep guessing the game number until the user enters correct value.*/
+Ask the user to keep guessing the game number until the user enters correct value.
 let gameNum = 25;
 let userNum = prompt("Guess the Gamenumber :")
 while(userNum != gameNum ){
     userNum = prompt("You guessed it wrong , Guess Again :");
 }
-console.log("Congratulation! You Entered the Correct Number")
+console.log("Congratulation! You Entered the Correct Number")*/
+
+
+/*Generate a username for the user with the help of prompt .
+the username will start with '@', followed with username and 
+end with the username length.*/ 
+
+let str = prompt("Enter Your Name :");
+let FullName = "@" + str + str.length;
+console.log(FullName);
