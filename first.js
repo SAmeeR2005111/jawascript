98fe8eb0d9d7bf3ad2e1b5cd0af2aa409a58fdf4
SAmeeR2005111 <sameerydv1920@gmail.com>
2024-12-09 15:58:59 +0530

    Created a game, Where the user guess the no. until the guessed no. is right

--- first.js
+++ first.js
@@ -1,13 +1,22 @@
 console.log("Welcome");
 
 // print all the no. from 0-100.
-for(i=0;i<=100;i++){
-    console.log(i);
-}
+//for(i=0;i<=100;i++){
+//    console.log(i);
+//}
 
 //print all the even no. from 0-100.
-for( num = 0 ; num <= 100 ; num++ ) {
-    if(num%2===0) {
-      console.log("Even no. =",num);
-    }
+//for( num = 0 ; num <= 100 ; num++ ) {
+//    if(num%2===0) {
+//      console.log("Even no. =",num);
+//    }
+//}
+
+/*creating a game where you start with any random game number. 
+Ask the user to keep guessing the game number until the user enters correct value.*/
+let gameNum = 25;
+let userNum = prompt("Guess the Gamenumber :")
+while(userNum != gameNum ){
+    userNum = prompt("You guessed it wrong , Guess Again :");
 }
+console.log("Congratulation! You Entered the Correct Number")
