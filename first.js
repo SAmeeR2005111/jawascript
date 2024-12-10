72f8f53f8b4bac3de09573f1ddd83da062ac9661
SAmeeR2005111 <sameerydv1920@gmail.com>
2024-12-10 18:47:43 +0530

    Solved some questions

--- first.js
+++ first.js
@@ -24,8 +24,39 @@ console.log("Congratulation! You Entered the Correct Number")*/
 
 /*Generate a username for the user with the help of prompt .
 the username will start with '@', followed with username and 
-end with the username length.*/ 
+end with the username length. 
 
 let str = prompt("Enter Your Name :");
 let FullName = "@" + str + str.length;
-console.log(FullName);
+console.log(FullName);*/
+
+/*for a given array with prices of 5 items [250,645,300,900,50] 
+all item have an offer of 10% off on them. Change the array to 
+store the final price after applying the offer.*/
+
+let prices = [250,645,300,900,50];
+
+for(let i =0; i<prices.length; i++){
+    offer = prices[i]/10;
+    prices[i] -= offer;
+}
+
+console.log(prices);
+
+
+/* Create an array to store the companies > "Bloomberg", "Microsoft", 
+"Uber", "Google", "IBM", "Netflix". 
+a) Remove the first company from the array.
+b) Remove "Uber" and add "OLA" in its place.
+c) Add Amazon at the end. */
+
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"]
+  
+companies.push("Amazon"); // Third Part of the question.
+console.log(companies);
+
+companies.shift(); // First part of the question.
+console.log(companies);
+
+companies.splice(2,1,"OLA"); // Second part of the question. also the index is changed due to previous shift.
+console.log(companies);
